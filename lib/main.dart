ab9bbe886683be6228c4e3d676bd1da51551c0eb
Mario <marioghbk@outlook.com>
2022-04-06 08:36:27 -0300

    scaffold_test

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Livro sobre Flutter',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nosso aplicativo'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Vezes que o botão foi apertado',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepPurple,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.purple,
+        child: Container(
+            height: 100,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.add_box, color: Colors.white),
+                      onPressed: () {
+                        alert('Adicionei qualquer coisa');
+                      }),
+                  IconButton(
+                      icon: Icon(Icons.add_a_photo, color: Colors.white),
+                      onPressed: () {
+                        alert('Adicionei uma foto');
+                      })
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+void alert(String message) {
+  print(message);
+}
